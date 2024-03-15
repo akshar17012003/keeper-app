29f38d137e6b9e4ab6de5a6571c53af73c31e584
akshar17012003 <aksharg17@gmail.com>
2024-03-15 08:18:49 +0530

    changesdone

--- keeper-app/app/(marketing)/_components/logo.tsx
+++ keeper-app/app/(marketing)/_components/logo.tsx
@@ -25,9 +25,7 @@ export const Logo = () => {
         alt="Logo"
         className="hidden dark:block"
       />
-      <p className={cn("font-semibold", font.className)}>
-        Jotion
-      </p>
+      <p className={cn("font-semibold", font.className)}>Keeper-App</p>
     </div>
-  )
+  );
 }
